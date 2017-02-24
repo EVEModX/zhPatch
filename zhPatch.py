b2e2b5c2a0e0e250621870ae3e45307c4f669504
Yiming Wang <wangym5106@gmail.com>
2017-02-24 12:13:33 +0800

    Add support for important name tooltip. Fix font settings.

--- zhPatch.py
+++ zhPatch.py
@@ -1,13 +1,15 @@
+# Version: 20170223
 # Website: http://zhpatch.evemodx.com/
 # QQ Group Number: 494245573
 
 import localization
-import localization.const
+import localization.internalUtil
 import localization.localizationBase
+
 import logmodule
 
 def patch_localization():
-    
+
     def _ReadLocalizationLanguagePickles_decorator(func):
         def wrapper(self, prefix, supportedLanguages, dataType):
             ret = func(self, prefix, supportedLanguages, dataType)
@@ -17,9 +19,11 @@ def patch_localization():
 
     def GetByMessageID_decorator(func):
         def wrapper(self, messageID, languageID=None, **kwargs):
-            ret = func(self, messageID, languageID='zh', **kwargs)
+            ret = None
+            if not languageID or languageID != 'en-us' or localization.internalUtil.GetLanguageID() == 'en-us':
+                ret = func(self, messageID, languageID='zh', **kwargs)
             if not ret or ret.startswith('[no '):
-                ret = func(self, messageID, languageID=languageID, **kwargs)
+                ret = func(self, messageID, languageID='en-us', **kwargs)
             return ret
         return wrapper
 
@@ -30,6 +34,12 @@ def patch_localization():
     localization.GetByMessageID = localization.LOCALIZATION_REF.GetByMessageID
 
 
+def patch_font():
+    from carbonui import languageConst, fontconst
+    fontconst.FONTFAMILY_PER_WINDOWS_LANGUAGEID[languageConst.LANG_JAPANESE] = fontconst.FONTFAMILY_PER_WINDOWS_LANGUAGEID[languageConst.LANG_CHINESE]
+
+
 patch_localization()
+patch_font()
 
 __import__('autoexec_client')
